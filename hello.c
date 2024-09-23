a9638eea3ba67b37c7e1016179aeb806e4bbc393
Harsha Vardhan <76719889+harshapk699@users.noreply.github.com>
2024-09-23 11:20:36 +0530

    Scotty (#1)
    Co-authored-by: Sumanth Goli <106972969+sumanthgoli0@users.noreply.github.com>

--- hello.c
+++ hello.c
@@ -4,5 +4,6 @@ void main()
 	printf("Hello World\n");
 	printf("Harvar Modified\n");
 	printf("jarvis replaced with friday\n");
+	printf("Today my Scooty got Punctured very difficult\n");
 	printf("BNO055 is integrated ion Dashpod\n");
 }
