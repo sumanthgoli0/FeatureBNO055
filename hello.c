470b2a9314a348e84fb41f9ffc2d586e0da4dad7
Harsha Vardhan <aravelliharshavardhan@gmail.com>
2024-09-23 11:24:11 +0530

    4 lines modified

--- hello.c
+++ hello.c
@@ -5,8 +5,8 @@ void main()
 	printf("Harvar Modified\n");
 	printf("jarvis replaced with friday\n");
 	printf("Today my Scooty got Punctured very difficult\n");
-	printf("Hello World\n");
-	printf("Harvar Modified\n");
-	printf("jarvis replaced with friday\n");
-	printf("Today my Scooty got Punctured very difficult\n");
+	printf("Hello Worl\n");
+	printf("Harvar Modifie\n");
+	printf("jarvis replaced with frday\n");
+	printf("Today my Scooty got Punctured\n");
 }
