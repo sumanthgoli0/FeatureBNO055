53b5b672c7abd4b667580b5805462d5d494aa8ea
Sumanth Goli <106972969+sumanthgoli0@users.noreply.github.com>
2024-09-23 11:20:46 +0530

    Revert "Scotty (#1)"
    This reverts commit a9638eea3ba67b37c7e1016179aeb806e4bbc393.

--- hello.c
+++ hello.c
@@ -4,6 +4,5 @@ void main()
 	printf("Hello World\n");
 	printf("Harvar Modified\n");
 	printf("jarvis replaced with friday\n");
-	printf("Today my Scooty got Punctured very difficult\n");
 	printf("BNO055 is integrated ion Dashpod\n");
 }
